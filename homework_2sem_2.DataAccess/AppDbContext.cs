7a39a315d92c3b4dfc9ba39f711b432d4a11cb8d
arinagomza <a11r221nir3@gmail.com>
2025-05-01 16:59:10 +0300

    Add DbContext

---
+++ homework_2sem_2.DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using homework_2sem_2.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace homework_2sem_2.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Lottery> Lotteries { get; set; }
+
+        public DbSet<Participant> Participants { get; set; }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
